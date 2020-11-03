ba8a5af0f05023ed3a236572f624e515fef0b0e9
Taher7013 <67517051+Taher7013@users.noreply.github.com>
2020-11-03 22:23:44 +0000

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,6 @@
 import subprocess
 
-data = suprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
 profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
 for i in profiles:
     results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
@@ -8,5 +8,5 @@ for i in profiles:
     try:
         print ("{:<30}|  {:<}".format(i, results[0]))
     except IndexError:
-        print ("{:<30}|
+        print ("{:<30}|  {:<}".format(i,""))
         input("")
